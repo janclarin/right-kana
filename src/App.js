8f2d39be079395f72b71906453223934308a5066
Jan Clarin <hi@janclarin.com>
2017-06-25 02:14:57 -0600

    Display basic table of kana

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import KanaBox from './KanaBox';
+import { KANA } from './constants/kana';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          KANA.map(kanaInfo => {
+            const kana = kanaInfo['hiragana'];
+            const romaji = kanaInfo['romaji'];
+            return <KanaBox kana={kana} romaji={romaji} />
+          })
+        }
       </div>
     );
   }
