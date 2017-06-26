71620fb8dd99e936c2b32fe88512c935f653499c
Jan Clarin <hi@janclarin.com>
2017-06-26 17:06:57 -0600

    Separate kana array into hiragana and katakana

--- src/constants/kana.js
+++ src/constants/kana.js
@@ -1,3 +1,146 @@
+/* Contains blocks for hiragana and katakana. */
+export const HIRAGANA = [
+  [
+    { 'romaji': 'a', 'kana': 'あ'},
+    { 'romaji': 'i', 'kana': 'い'},
+    { 'romaji': 'u', 'kana': 'う'},
+    { 'romaji': 'e', 'kana': 'え'},
+    { 'romaji': 'o', 'kana': 'お'},
+  ],
+  [
+    { 'romaji': 'ka', 'kana': 'か'},
+    { 'romaji': 'ki', 'kana': 'き'},
+    { 'romaji': 'ku', 'kana': 'く'},
+    { 'romaji': 'ke', 'kana': 'け'},
+    { 'romaji': 'ko', 'kana': 'こ'},
+  ],
+  [
+    { 'romaji': 'sa', 'kana': 'さ'},
+    { 'romaji': 'shi', 'kana': 'し'},
+    { 'romaji': 'su', 'kana': 'す'},
+    { 'romaji': 'se', 'kana': 'せ'},
+    { 'romaji': 'so', 'kana': 'そ'},
+  ],
+  [
+    { 'romaji': 'ta', 'kana': 'た'},
+    { 'romaji': 'chi', 'kana': 'ち'},
+    { 'romaji': 'tsu', 'kana': 'つ'},
+    { 'romaji': 'te', 'kana': 'て'},
+    { 'romaji': 'to', 'kana': 'と'},
+  ],
+  [
+    { 'romaji': 'na', 'kana': 'な'},
+    { 'romaji': 'ni', 'kana': 'に'},
+    { 'romaji': 'nu', 'kana': 'ぬ'},
+    { 'romaji': 'ne', 'kana': 'ね'},
+    { 'romaji': 'no', 'kana': 'の'},
+  ],
+  [
+    { 'romaji': 'ha', 'kana': 'は'},
+    { 'romaji': 'hi', 'kana': 'ひ'},
+    { 'romaji': 'fu', 'kana': 'ふ'},
+    { 'romaji': 'he', 'kana': 'へ'},
+    { 'romaji': 'ho', 'kana': 'ほ'},
+  ],
+  [
+    { 'romaji': 'ra', 'kana': 'ら'},
+    { 'romaji': 'ri', 'kana': 'り'},
+    { 'romaji': 'ru', 'kana': 'る'},
+    { 'romaji': 're', 'kana': 'れ'},
+    { 'romaji': 'ro', 'kana': 'ろ'},
+  ],
+  [
+    { 'romaji': 'ma', 'kana': 'ま'},
+    { 'romaji': 'mi', 'kana': 'み'},
+    { 'romaji': 'mu', 'kana': 'む'},
+    { 'romaji': 'me', 'kana': 'め'},
+    { 'romaji': 'mo', 'kana': 'も'},
+  ],
+  [
+    { 'romaji': 'ya', 'kana': 'や'},
+    { 'romaji': 'yu', 'kana': 'ゆ'},
+    { 'romaji': 'yo', 'kana': 'よ'},
+  ],
+  [
+    { 'romaji': 'wa', 'kana': 'わ'},
+    { 'romaji': 'wo', 'kana': 'を'},
+  ],
+  [
+    { 'romaji': 'n', 'kana': 'ん'}
+  ]
+];
+
+export const KATAKANA = [
+  [
+    { 'romaji': 'a', 'kana': 'ア' },
+    { 'romaji': 'i', 'kana': 'イ' },
+    { 'romaji': 'u', 'kana': 'ウ' },
+    { 'romaji': 'e', 'kana': 'エ' },
+    { 'romaji': 'o', 'kana': 'オ' },
+  ],
+  [
+    { 'romaji': 'ka', 'kana': 'カ' },
+    { 'romaji': 'ki', 'kana': 'キ' },
+    { 'romaji': 'ku', 'kana': 'ク' },
+    { 'romaji': 'ke', 'kana': 'ケ' },
+    { 'romaji': 'ko', 'kana': 'コ' },
+  ],
+  [
+    { 'romaji': 'sa', 'kana': 'サ' },
+    { 'romaji': 'shi', 'kana': 'シ' },
+    { 'romaji': 'su', 'kana': 'ス' },
+    { 'romaji': 'se', 'kana': 'セ' },
+    { 'romaji': 'so', 'kana': 'ソ' },
+  ],
+  [
+    { 'romaji': 'ta', 'kana': 'タ' },
+    { 'romaji': 'chi', 'kana': 'チ' },
+    { 'romaji': 'tsu', 'kana': 'ツ' },
+    { 'romaji': 'te', 'kana': 'テ' },
+    { 'romaji': 'to', 'kana': 'ト' },
+  ],
+  [
+    { 'romaji': 'na', 'kana': 'ナ' },
+    { 'romaji': 'ni', 'kana': 'ニ' },
+    { 'romaji': 'nu', 'kana': 'ヌ' },
+    { 'romaji': 'ne', 'kana': 'ネ' },
+    { 'romaji': 'no', 'kana': 'ノ' },
+  ],
+  [
+    { 'romaji': 'ha', 'kana': 'ハ' },
+    { 'romaji': 'hi', 'kana': 'ヒ' },
+    { 'romaji': 'fu', 'kana': 'フ' },
+    { 'romaji': 'he', 'kana': 'ヘ' },
+    { 'romaji': 'ho', 'kana': 'ホ' },
+  ],
+  [
+    { 'romaji': 'ra', 'kana': 'ラ' },
+    { 'romaji': 'ri', 'kana': 'リ' },
+    { 'romaji': 'ru', 'kana': 'ル' },
+    { 'romaji': 're', 'kana': 'レ' },
+    { 'romaji': 'ro', 'kana': 'ロ' },
+  ],
+  [
+    { 'romaji': 'ma', 'kana': 'マ' },
+    { 'romaji': 'mi', 'kana': 'ミ' },
+    { 'romaji': 'mu', 'kana': 'ム' },
+    { 'romaji': 'me', 'kana': 'メ' },
+    { 'romaji': 'mo', 'kana': 'モ' },
+  ],
+  [
+    { 'romaji': 'ya', 'kana': 'ヤ' },
+    { 'romaji': 'yu', 'kana': 'ユ' },
+    { 'romaji': 'yo', 'kana': 'ヨ' },
+  ],
+  [
+    { 'romaji': 'wa', 'kana': 'ワ' },
+    { 'romaji': 'wo', 'kana': 'ヲ' },
+  ],
+  [
+    { 'romaji': 'n', 'kana': 'ン' }
+  ]
+];
+
 export const KANA = [
   [
     { 'romaji': 'a', 'hiragana': 'あ', 'katakana': 'ア' },
