936fad9da3188f334bf277c1e5a9039ce515c17e
Jan Clarin <hi@janclarin.com>
2017-06-27 14:30:09 -0600

    Surround gif with div to keep modal height when loading

--- src/KanaInfoModal.js
+++ src/KanaInfoModal.js
@@ -4,13 +4,19 @@ import { Modal, Image } from 'react-bootstrap';
 class KanaInfoModal extends Component {
   render() {
     const { kana, romaji, gifUrl, ...other } = this.props;
+    const gifDivStyle = {
+      height: 240
+    };
+
     return (
       <Modal {...other}>
         <Modal.Header closeButton>
           <Modal.Title>{kana} ({romaji})</Modal.Title>
         </Modal.Header>
         <Modal.Body className="text-center">
-          <Image src={gifUrl} />
+          <div style={gifDivStyle}>
+            <Image src={gifUrl} />
+          </div>
         </Modal.Body>
       </Modal>
     );
