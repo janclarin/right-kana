965c0fd98e9612f15e31add778e1acef70301fee
Jan Clarin <hi@janclarin.com>
2017-06-26 21:01:33 -0600

    Add dummy kana elements for consistency

--- src/constants/kana.js
+++ src/constants/kana.js
@@ -58,15 +58,24 @@ export const HIRAGANA = [
   ],
   [
     { 'romaji': 'ya', 'kana': 'や'},
+    { 'romaji': '', 'kana': ''},
     { 'romaji': 'yu', 'kana': 'ゆ'},
+    { 'romaji': '', 'kana': ''},
     { 'romaji': 'yo', 'kana': 'よ'},
   ],
   [
     { 'romaji': 'wa', 'kana': 'わ'},
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
     { 'romaji': 'wo', 'kana': 'を'},
   ],
   [
-    { 'romaji': 'n', 'kana': 'ん'}
+    { 'romaji': 'n', 'kana': 'ん'},
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
   ]
 ];
 
@@ -129,85 +138,23 @@ export const KATAKANA = [
   ],
   [
     { 'romaji': 'ya', 'kana': 'ヤ' },
+    { 'romaji': '', 'kana': ''},
     { 'romaji': 'yu', 'kana': 'ユ' },
+    { 'romaji': '', 'kana': ''},
     { 'romaji': 'yo', 'kana': 'ヨ' },
   ],
   [
     { 'romaji': 'wa', 'kana': 'ワ' },
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
     { 'romaji': 'wo', 'kana': 'ヲ' },
   ],
   [
-    { 'romaji': 'n', 'kana': 'ン' }
-  ]
-];
-
-export const KANA = [
-  [
-    { 'romaji': 'a', 'hiragana': 'あ', 'katakana': 'ア' },
-    { 'romaji': 'i', 'hiragana': 'い', 'katakana': 'イ' },
-    { 'romaji': 'u', 'hiragana': 'う', 'katakana': 'ウ' },
-    { 'romaji': 'e', 'hiragana': 'え', 'katakana': 'エ' },
-    { 'romaji': 'o', 'hiragana': 'お', 'katakana': 'オ' },
-  ],
-  [
-    { 'romaji': 'ka', 'hiragana': 'か', 'katakana': 'カ' },
-    { 'romaji': 'ki', 'hiragana': 'き', 'katakana': 'キ' },
-    { 'romaji': 'ku', 'hiragana': 'く', 'katakana': 'ク' },
-    { 'romaji': 'ke', 'hiragana': 'け', 'katakana': 'ケ' },
-    { 'romaji': 'ko', 'hiragana': 'こ', 'katakana': 'コ' },
-  ],
-  [
-    { 'romaji': 'sa', 'hiragana': 'さ', 'katakana': 'サ' },
-    { 'romaji': 'shi', 'hiragana': 'し', 'katakana': 'シ' },
-    { 'romaji': 'su', 'hiragana': 'す', 'katakana': 'ス' },
-    { 'romaji': 'se', 'hiragana': 'せ', 'katakana': 'セ' },
-    { 'romaji': 'so', 'hiragana': 'そ', 'katakana': 'ソ' },
-  ],
-  [
-    { 'romaji': 'ta', 'hiragana': 'た', 'katakana': 'タ' },
-    { 'romaji': 'chi', 'hiragana': 'ち', 'katakana': 'チ' },
-    { 'romaji': 'tsu', 'hiragana': 'つ', 'katakana': 'ツ' },
-    { 'romaji': 'te', 'hiragana': 'て', 'katakana': 'テ' },
-    { 'romaji': 'to', 'hiragana': 'と', 'katakana': 'ト' },
-  ],
-  [
-    { 'romaji': 'na', 'hiragana': 'な', 'katakana': 'ナ' },
-    { 'romaji': 'ni', 'hiragana': 'に', 'katakana': 'ニ' },
-    { 'romaji': 'nu', 'hiragana': 'ぬ', 'katakana': 'ヌ' },
-    { 'romaji': 'ne', 'hiragana': 'ね', 'katakana': 'ネ' },
-    { 'romaji': 'no', 'hiragana': 'の', 'katakana': 'ノ' },
-  ],
-  [
-    { 'romaji': 'ha', 'hiragana': 'は', 'katakana': 'ハ' },
-    { 'romaji': 'hi', 'hiragana': 'ひ', 'katakana': 'ヒ' },
-    { 'romaji': 'fu', 'hiragana': 'ふ', 'katakana': 'フ' },
-    { 'romaji': 'he', 'hiragana': 'へ', 'katakana': 'ヘ' },
-    { 'romaji': 'ho', 'hiragana': 'ほ', 'katakana': 'ホ' },
-  ],
-  [
-    { 'romaji': 'ra', 'hiragana': 'ら', 'katakana': 'ラ' },
-    { 'romaji': 'ri', 'hiragana': 'り', 'katakana': 'リ' },
-    { 'romaji': 'ru', 'hiragana': 'る', 'katakana': 'ル' },
-    { 'romaji': 're', 'hiragana': 'れ', 'katakana': 'レ' },
-    { 'romaji': 'ro', 'hiragana': 'ろ', 'katakana': 'ロ' },
-  ],
-  [
-    { 'romaji': 'ma', 'hiragana': 'ま', 'katakana': 'マ' },
-    { 'romaji': 'mi', 'hiragana': 'み', 'katakana': 'ミ' },
-    { 'romaji': 'mu', 'hiragana': 'む', 'katakana': 'ム' },
-    { 'romaji': 'me', 'hiragana': 'め', 'katakana': 'メ' },
-    { 'romaji': 'mo', 'hiragana': 'も', 'katakana': 'モ' },
-  ],
-  [
-    { 'romaji': 'ya', 'hiragana': 'や', 'katakana': 'ヤ' },
-    { 'romaji': 'yu', 'hiragana': 'ゆ', 'katakana': 'ユ' },
-    { 'romaji': 'yo', 'hiragana': 'よ', 'katakana': 'ヨ' },
-  ],
-  [
-    { 'romaji': 'wa', 'hiragana': 'わ', 'katakana': 'ワ' },
-    { 'romaji': 'wo', 'hiragana': 'を', 'katakana': 'ヲ' },
-  ],
-  [
-    { 'romaji': 'n', 'hiragana': 'ん', 'katakana': 'ン' }
+    { 'romaji': 'n', 'kana': 'ン' },
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
+    { 'romaji': '', 'kana': ''},
   ]
 ];
